26a3ce645fba8436c9bdeecbb9c95c00b9ce2796
Daniil Perov <danilperov796@gmail.com>
2025-02-24 16:49:53 +0300

    Parse schedule with sirinium

--- index.js
+++ index.js
@@ -1,44 +1,55 @@
 const { writeFileSync } = require("fs");
 const { createEvents } = require("ics");
+const sirinium = require("sirinium");
 
-const scheduleRawJson = require("./data/schedule.json");
+const client = new sirinium.Client();
 
 const schedules = {
     "apple": [],
     "other": []
 };
-for (const i in scheduleRawJson) {
-    for (const e of scheduleRawJson[i]) {
-        const dateSplit = e["date"].split(".");
+
+const generate = async () => {
+    await client.getInitialData();
+
+    const schedule = await client.getGroupSchedule(process.env.SCHEDULE_GROUP ?? "К0609-24");
+
+    for (const i of schedule) {
+        const dateSplit = i["date"].split(".");
+
+        const teacherName =
+            Object.values(i["teachers"]).length !== 0 ? Object.values(i["teachers"])[0]["fio"] : "–";
 
         const obj = {
             calName: "Sirius University",
 
-            title: `${e["discipline"]} (${e["groupType"]})`,
+            title: `${i["discipline"]} (${i["groupType"]})`,
 
             organizer: {
-                name: Object.values(e["teachers"])[0]["fio"],
+                name: teacherName,
                 email: "info@siriusuniversity.ru"
             },
 
-            location: e["classroom"],
+            location: i["classroom"],
 
-            start: [dateSplit[2], dateSplit[1], dateSplit[0], ...e["startTime"].split(":")]
+            start: [dateSplit[2], dateSplit[1], dateSplit[0], ...i["startTime"].split(":")]
                 .map((d) => { return parseInt(d) }),
-            end: [dateSplit[2], dateSplit[1], dateSplit[0], ...e["endTime"].split(":")]
+            end: [dateSplit[2], dateSplit[1], dateSplit[0], ...i["endTime"].split(":")]
                 .map((d) => { return parseInt(d) }),
         }
 
         schedules.apple.push(obj);
 
-        obj.description = `Преподаватель: ${Object.values(e["teachers"])[0]["fio"]}`;
+        obj.description = `Преподаватель: ${teacherName}`;
         schedules.other.push(obj);
+    }
+
+    for (const s in schedules) {
+        const { error, value } = createEvents(schedules[s]);
+        if (error) throw error;
+
+        writeFileSync(`${__dirname}/schedule_${s}.ics`, value);
     }
 }
 
-for (const s in schedules) {
-    const { error, value } = createEvents(schedules[s]);
-    if (error) throw error;
-
-    writeFileSync(`${__dirname}/schedule_${s}.ics`, value);
-}
+generate().then(() => console.log("Generation finished!"));
